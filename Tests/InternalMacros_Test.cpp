d2724876904a20dba97ef6d0e8301fb61b09605c
Syed Aman <syednashikaman@gmail.com>
2023-05-15 21:39:41 -0400

    Return 0 when COUNT_ARGS is empty. It doesn't work very well though

--- Tests/InternalMacros_Test.cpp
+++ Tests/InternalMacros_Test.cpp
@@ -1,9 +1,23 @@
 #include "../InternalMacros.h"
 #include "../TestingFramework.h"
 
+
+TEST(COUNT_ARGS_internal, ReturnsNumberOfArguments)
+{
+    EXPECT_EQ(COUNT_ARGS(1, 2, 3, 4, 5), 5);
+    EXPECT_EQ(COUNT_ARGS(), 0);
+}
+
+TEST(COUNT_ARG_PAIRS_internal, ReturnsNumberOfArgumentPairs)
+{
+    EXPECT_EQ(COUNT_ARG_PAIRS(int, a), 1);
+    EXPECT_EQ(COUNT_ARG_PAIRS(int, float), 1);
+    EXPECT_EQ(COUNT_ARG_PAIRS(float, arg1, int, arg2), 2);
+}
+
 TEST(CONCAT_internal, ConcatenatesLeftWithRight)
 {
-    EXPECT_EQ(CONCAT_internal(1,2), 12);
+    EXPECT_EQ(CONCAT_internal(1, 2), 12);
 }
 
 TEST(IS_EMPTY_internal, ReturnsTheTestTokenIfEmptyAndConcatenatedOtherwise)
@@ -13,22 +27,8 @@ TEST(IS_EMPTY_internal, ReturnsTheTestTokenIfEmptyAndConcatenatedOtherwise)
     EXPECT_EQ(IS_EMPTY_internal(1), 1231);
 }
 
-TEST(CHECK_IS_EMPTY_internal, works)
+TEST(CHECK_IS_EMPTY_internal, ReturnsTrueIfEmpty)
 {
     EXPECT_EQ(CHECK_IS_EMPTY_internal(), true);
     EXPECT_EQ(CHECK_IS_EMPTY_internal(99), false);
-}
-
-TEST(COUNT_ARGS_internal, ReturnsNumberOfArguments)
-{
-    EXPECT_EQ(COUNT_ARGS(1, 2, 3, 4, 5), 5);
-    EXPECT_EQ(COUNT_ARGS("one", "two", "three"), 3);
-    EXPECT_EQ(COUNT_ARGS('a', 'b', 'c', 'd'), 4);
-    EXPECT_EQ(COUNT_ARGS(1, "two", 3.0, '4'), 4);
-}
-
-TEST(COUNT_ARG_PAIRS_internal, ReturnsNumberOfArgumentPairs)
-{
-    EXPECT_EQ(COUNT_ARG_PAIRS(int, a), 1);
-    EXPECT_EQ(COUNT_ARG_PAIRS(float, arg1, int, arg2), 2);
 }
